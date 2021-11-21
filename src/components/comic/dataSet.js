fbd67a69bc08a93429cdc944dbe8f0fbce6325df
Daniel <tamayodesigne@gmail.com>
2021-11-21 13:57:15 -0500

    add emojis

--- src/components/comic/dataSet.js
+++ src/components/comic/dataSet.js
@@ -1,11 +1,11 @@
 export const rates = [
-    { id: 'star5', message: 'Amo este comic', name: 'rating', value: '5', class: 'full' },
-    { id: 'star4.5', message: 'Amo este comic', name: 'rating', value: '4.5', class: 'half' },
-    { id: 'star4', message: 'Me encanta', name: 'rating', value: '4', class: 'full' },
-    { id: 'star3.5', message: 'Es divertido', name: 'rating', value: '3.5', class: 'half' },
-    { id: 'star3', message: 'Es divertido', name: 'rating', value: '3', class: 'full' },
-    { id: 'star2.5', message: 'No me gusta', name: 'rating', value: '2.5', class: 'half' },
-    { id: 'star2', message: 'No me gusta', name: 'rating', value: '2', class: 'full' },
-    { id: 'star1.5', message: 'Terrible', name: 'rating', value: '1.5', class: 'half' },
-    { id: 'star1', message: 'Terrible', name: 'rating', value: '1', class: 'full' }
+    { id: 'star5', message: 'Amo este comic 😍', name: 'rating', value: '5', class: 'full' },
+    { id: 'star4.5', message: 'Amo este comic 😍', name: 'rating', value: '4.5', class: 'half' },
+    { id: 'star4', message: 'Me encanta 🤩', name: 'rating', value: '4', class: 'full' },
+    { id: 'star3.5', message: 'Es divertido 😊', name: 'rating', value: '3.5', class: 'half' },
+    { id: 'star3', message: 'Es divertido 😊 ', name: 'rating', value: '3', class: 'full' },
+    { id: 'star2.5', message: 'No me gusta 😪', name: 'rating', value: '2.5', class: 'half' },
+    { id: 'star2', message: 'No me gusta 😪', name: 'rating', value: '2', class: 'full' },
+    { id: 'star1.5', message: 'Terrible 😣', name: 'rating', value: '1.5', class: 'half' },
+    { id: 'star1', message: 'Terrible 😣', name: 'rating', value: '1', class: 'full' }
 ]
